b7d170ccf1c944cf640d52c54e6b529df5cee7f4
Tedozi Manson <duyphan.developer@gmail.com>
2016-04-29 01:09:30 +0700

    [modify] - Make content max string to 500000

--- app/Models/ProductCategoryContent.php
+++ app/Models/ProductCategoryContent.php
@@ -29,7 +29,7 @@ class ProductCategoryContent extends AbstractModel
         'slug' => 'required|max:255|unique:product_category_contents',
         'language_id' => 'min:1|integer|required',
         'description' => 'max:1000',
-        'content' => 'max:5000|string',
+        'content' => 'max:500000|string',
         'status' => 'integer|required',
         'thumbnail' => 'string|max:255',
         'tags' => 'string|max:255',
